6d36930ad1bfda15d262d4fdbe48f671bdaf3766
Irven Aelbrecht <irven.aelbrecht@gmail.com>
2022-07-07 14:27:56 +0200

    :sparkles: add a test for workflow condition

---
+++ src/__tests__/condition.test.ts
@@ -0,0 +1,43 @@
+import { Worker } from '@temporalio/worker';
+import { WorkflowClient } from '@temporalio/client';
+import { createWorker } from '../worker';
+import { testCondition, testSignal } from '../workflows';
+import { terminateRunningTestWorkflow } from '../utils';
+
+const taskQueue = 'condition';
+
+describe('Test hello world workflow with Temporal cluster.', () => {
+    let worker: Worker;
+    let workerRunning: Promise<void>;
+    beforeAll(async () => {
+        worker = await createWorker(taskQueue);
+        workerRunning = worker.run(); // Do not await, because we will shut it down after the test.
+    }, 1000 * 60);
+    afterAll(async () => {
+        worker.shutdown();
+        await workerRunning; // Wait for shutdown.
+    });
+    it(
+        'Run test.',
+        async () => {
+            const workflowId = 'test-hello';
+            const client = new WorkflowClient();
+            await terminateRunningTestWorkflow(client, workflowId);
+
+            await client.start(testCondition, {
+                taskQueue,
+                workflowId
+            });
+
+            await new Promise( f => setTimeout(f, 5000));
+
+            const handle = client.getHandle(workflowId);
+            await handle.signal(testSignal);
+
+            const res = await handle.result(); // Do not forget to wait on result.
+
+            expect(res).toBe("Hello, test!");
+        },
+        1000 * 100
+    );
+});
--- src/workflows.ts
+++ src/workflows.ts
@@ -1,4 +1,4 @@
-import { proxyActivities } from '@temporalio/workflow';
+import { proxyActivities, defineSignal, setHandler, condition, sleep } from '@temporalio/workflow';
 // Only import the activity types
 import type * as activities from './activities';
 
@@ -9,4 +9,19 @@ const { greet } = proxyActivities<typeof activities>({
 /** A workflow that simply calls an activity */
 export async function example(name: string): Promise<string> {
     return await greet(name);
+}
+
+export const testSignal = defineSignal("test");
+
+export async function testCondition() {
+  let state = 'WAITING_ON_INPUT';
+  setHandler(testSignal, () => {
+    state = 'UPDATE';
+  });
+
+  const conditionPromise = condition(() => state === 'UPDATE', '10s' );
+  await sleep('1s');
+  const res = await greet("test");
+  await conditionPromise;
+  return res;
 }
