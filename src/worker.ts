02ed4a1e2a6a92a5a86942c772211df67435c40c
Irven Aelbrecht <irven.aelbrecht@gmail.com>
2022-08-29 13:24:23 +0200

    :bug: apply webpackConfigHook correctly

--- src/worker.ts
+++ src/worker.ts
@@ -1,4 +1,5 @@
 import path from 'path';
+import type { Configuration as WebpackConfiguration } from 'webpack';
 
 import { Worker } from '@temporalio/worker';
 import * as activities from 'activities';
@@ -9,9 +10,16 @@ export function defaultWorkerOptions() {
       activities,
       shutdownGraceTime: '10s',
       debugMode: true,
-      webpackConfigHook: {
-        resolve : {
-          roots: [path.dirname(__filename)]
+      bundlerOptions: {
+        webpackConfigHook: (config: WebpackConfiguration) => {
+          if (!config.resolve) config.resolve = {};
+          if (config.resolve.modules && config.resolve.modules.length > 0) {
+            config.resolve.modules.push(path.resolve(__dirname, '../'));
+          } else {
+            config.resolve.modules = [path.resolve(__dirname, '../')];
+          }
+          console.log(config);
+          return config;
         }
       }
     };
